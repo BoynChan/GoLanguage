2b6d3f4c1292968e8ee6b08d80a1b217d07e5b54
KaserChan <chankaser@gmail.com>
2019-11-02 19:54:53 +0800

    结构体的toString方法

---
+++ src/main/goblog/toStringInGo.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Student struct {
+	Name string
+	Age int
+}
+
+func (s Student) String() string {
+	return fmt.Sprintf("%v (%v years)",s.Name,s.Age)
+}
+
+func main(){
+	s := Student{"Boyn",21}
+	fmt.Println(s)
+}
